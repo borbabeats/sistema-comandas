d95e968923a7501e03e8607150eafe97f7f781b2
Maicon Borba <68969007+borbabeats@users.noreply.github.com>
2025-08-27 14:35:31 -0300

    Update index.js

--- backend/database/index.js
+++ backend/database/index.js
@@ -111,7 +111,7 @@ function getDatabaseConfig() {
     host: process.env.DB_HOST || 'localhost',
     port: parseInt(process.env.DB_PORT || '5432'),
     username: process.env.DB_USERNAME || 'admin',
-    password: process.env.DB_PASSWORD || 'pa&@)s28',
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME || 'comandas',
     synchronize: false,
     logging: true,
